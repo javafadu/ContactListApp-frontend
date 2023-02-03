2aa401da7eb53195f3e359766745b4adab2d2071
javafadu <javafadu@gmail.com>
2023-02-04 02:13:10 +0300

    04feb0215

--- src/components/user/common/footer/footer.js
+++ src/components/user/common/footer/footer.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { settings } from "../../../../utils/settings";
+import "./footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Container className="footer" fluid>
+      <Container>
+        <Row>
+          <Col className="text-center text-md-right" md={6}>
+            <h4>Contact List App</h4>
+
+            <p>{settings.address}</p>
+            <p>{settings.siteUrl}</p>
+
+
+          </Col>
+          <Col className="text-center text-md-right" md={6}>
+            <h4>7/24 Contact With Us</h4>
+            <p>{settings.email}</p>
+            <p>{settings.phone1}</p>
+
+
+          </Col>
+        </Row>
+
+      </Container>
+    </Container>
+  )
 };
 
 export default Footer;
